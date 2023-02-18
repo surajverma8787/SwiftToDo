715b08528123cb8549738c0f0da7a1cbbae8fc40
Suraj <surajverma8787@gmail.com>
2023-02-18 18:16:46 +0530

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Suraj Verma on 18/02/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
